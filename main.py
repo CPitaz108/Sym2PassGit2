9022a9884508acb7a45db43fc3c0034cf6a425ad
CPitaz108 <44993848+CPitaz108@users.noreply.github.com>
2020-09-15 16:12:51 -0600

    Initial Flask Outline on API Generation

--- main.py
+++ main.py
@@ -12,18 +12,21 @@
 
 #Import Flask and Flask_Restful's API, Resource
 from flask import Flask;
-from flask_restful import Api, Resource;
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with;
+from flask_mysqldb import MySQL;
+from flask_sqlalchemy import SQLAlchemy;
 
 ##Create a Flash object called API with the parameter __name__ (Name of the python file)
 app = Flask(__name__);
 ##Creats a API object called API from the Flash object
 api = Api(app);
+app.config()
 
 ##Test class that defines a HelloWorld object. By defining the class HelloWorld, you can give it methods (A get method has already been established that
 # just returns a sample JSON object with a data key, and a value of "Hello World"
 class HelloWorld(Resource):
     def get(self):
-        return {"data": "Hello World"}
+        return {"data": "Hello World"};
 ##You can def PUT, def POST, def PATCH, GET, DELETE. to define other methods for this class that you may want (For updating an entire field, inserting, updating a single field, getting data from the DB, and removing records
 # respectively)
 
@@ -34,4 +37,4 @@ api.add_resource(HelloWorld, "/helloworld")
 
 ##If the function is the main.py (Which this is)
 if __name__ == "__main__":
-    app.run(debug=True);  asd;
+    app.run(debug=True);
