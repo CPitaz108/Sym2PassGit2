33b6a70fa9b18311a26887137b77171f0ec5b11f
LoganDuffy702 <32749000+LoganDuffy702@users.noreply.github.com>
2020-09-15 20:54:09 -0600

    Second Merge

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from flask_mysqldb import MySQL;
 app = Flask(__name__);
 
 app.config['MYSQL_USER'] = 'root';
-app.config['MYSQL_PASSWORD'] = '1234';
+app.config['MYSQL_PASSWORD'] = '1001494';
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_DB'] = 'sympass'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
